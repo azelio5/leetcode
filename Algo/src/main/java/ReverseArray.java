79f9efe4ac0a913f7750406fa65572f77698277e
azelio5 <a.alakbarov@gmail.com>
2025-04-24 10:20:17 +0300

    reverseArray

---
+++ Algo/src/main/java/ReverseArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void reverseArray(int[] arr) {
+        int i = 0, j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i <arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println(" ");
+        reverseArray(arr);
+        for (int i = 0; i <arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
