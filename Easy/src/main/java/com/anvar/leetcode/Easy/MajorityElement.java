9f73c823b8728edba4dc130f71d3c666427d44a4
azelio5 <a.alakbarov@gmail.com>
2025-05-15 11:22:38 +0300

    badMajority

--- Easy/src/main/java/com/anvar/leetcode/Easy/MajorityElement.java
+++ Easy/src/main/java/com/anvar/leetcode/Easy/MajorityElement.java
@@ -5,8 +5,25 @@ public class MajorityElement {
     public static void main(String[] args) {
         int[] arr = {3, 2, 3};
 
-        System.out.println(majorityElement(arr));
+        System.out.println(badMajorityElement(arr));
+
     }
+
+    public static int badMajorityElement(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > nums.length / 2) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
     public static int majorityElement(int[] nums) {
         int count = 0, candidate = 0;
         for (int num : nums) {
